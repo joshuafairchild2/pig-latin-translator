071d6a719980183084817aa5aa987e08fbcdad35
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-03-28 08:32:00 -0700

    make things a little simpler (no functional changes, just removing redundant code and shortening things up a bit/moving comments around)

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,24 @@
-//frontend
-$(function() {
-  $("form").submit(function(event) {
-    var inputString = $("#input").val();
-    var outputString = toPigLatin(inputString);
-    if (outputString === "") {
-      $('#output').show();
-      $('#output').text('Please enter a word or phrase');
-    } else {
-      $("#output").show();
-      $("#output").text(outputString);
-    }
-    event.preventDefault()
-  });
-});
-
-//backend
+//business logc
+//takes a letter as an argument, returns true if it is a vowel, false otherwise
 var checkForVowel = function(letter) {
-    var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-    for (var index = 0; index < vowels.length; index += 1) {
-      if (vowels[index] === letter) {
-        var isVowel = true;
-      }
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  var isVowel = false;
+  for (var index = 0; index < vowels.length; index += 1) {
+    if (vowels[index] === letter) {
+      var isVowel = true;
     }
-    if (isVowel === true) {
-      return true;
-    } else {
-      return false;
-    }
-} //function to check if letter is a vowel, return true/false accordingly
+  }
+  return isVowel;
+}
 
 var toPigLatin = function(inputString) {
-  var inputString = inputString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  var inputString = inputString.replace(/\s{2,}/g," ");
-  var individualWords = inputString.toLowerCase().split(' ');
-  //transform input to lowercase, remove punctuation, split input into an array of words
-
+  //transform input to lowercase, remove punctuation, split into an array of words
+  //initialize empty array to collect all of the translated words
+  var individualWords = inputString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').toLowerCase().split(' ');
   var resultArray = [];
-  //create empty array to collect all of the translated words
 
   individualWords.forEach(function(individualWord) {
+    //create an array of booleans representing "vowel" and "consonant" where true = vowel and false = consonant
     var vowelsArray = [];
     for (var i = 0; i < individualWord.length; i++) {
       if (individualWord[i] === "y" && i === 0) {
@@ -46,24 +26,43 @@ var toPigLatin = function(inputString) {
       } else {
         vowelsArray.push(checkForVowel(individualWord[i]));
       }
-    } //create an array of booleans representing "vowel" and "consonant" where true = vowel and false = consonant
-    console.log(vowelsArray);
-    if (!isNaN(parseFloat(individualWord))) {
-      resultArray.push(individualWord); //pass numbers through translator unchanged
-    }  else if (checkForVowel(individualWord[0]) && individualWord[0] !== "y") {
-      resultArray.push(individualWord + "ay");
-    } //translate a word that starts with a vowel
+    }
 
+    //pass numbers through translator unchanged
+    if (!isNaN(parseFloat(individualWord))) {
+      resultArray.push(individualWord);
+    }
+    //translate a word that starts with a vowel
+    if (checkForVowel(individualWord[0]) && individualWord[0] !== "y") {
+      resultArray.push(individualWord + "ay");
+    }
+
+    //iterate through a word, stopping when the next character is a vowel
     for (var index = 0; vowelsArray[index] === false; index++) {
-      if (individualWord[index] === "q" && individualWord[(index + 1)] === "u") { //translate a word containing a "q"
+      //translate a word containing a "q"
+      if (individualWord[index] === "q" && individualWord[(index + 1)] === "u") {
         resultArray.push((individualWord.substr(index + 2) + individualWord.substr(0, (index + 2))) + "ay");
-      } else if (vowelsArray[(index + 1)] === true) {
+      }
+      //translate a word that has 1 or more consonants (other than q) preceding first vowel
+      else if (vowelsArray[(index + 1)] === true) {
         resultArray.push((individualWord.substr(index + 1) + individualWord.substr(0, (index + 1))) + "ay");
-      } //translate a word that has 1 or more consonants (other than q) preceding first vowel
+      }
     }
   });
-
-  resultArray = resultArray.join(" ");
-  //concat the translated words back into a sentence
-  return resultArray;
+  return resultArray.join(" ");
 }
+
+//ui logic
+$(function() {
+  $("form").submit(function(event) {
+    var inputString = $("#input").val();
+    var outputString = toPigLatin(inputString);
+    if (outputString === "") {
+      alert('Please enter a word or phrase');
+    } else {
+      $("#output").show().text(outputString);
+    }
+    $(this).trigger('reset');
+    event.preventDefault();
+  });
+});
